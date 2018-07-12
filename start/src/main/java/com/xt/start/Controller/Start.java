d6b67ec71704812325108fdd8a6edc250a6e39ff
weiran <602208018@qq.com>
2018-07-12 22:37:04 +0800

    mybatis

--- start/src/main/java/com/xt/start/Controller/Start.java
+++ start/src/main/java/com/xt/start/Controller/Start.java
@@ -1,5 +1,9 @@
 package com.xt.start.Controller;
 
+import com.xt.start.Bean.Cat;
+import com.xt.start.dao.CatDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Start {
 
+  @Autowired private CatDB catDB;
+
   @RequestMapping(value = "/hello")
   public String hello() {
-    return "hello world";
+    return "hello world6";
+  }
+
+  @PostMapping(value = "/catinfo")
+  public void saveCatInfo() {
+    Cat cat = new Cat(1, "mimi", 1);
+    catDB.insertCatInfo(cat);
   }
 }
--- start/src/main/java/com/xt/start/StartApplication.java
+++ start/src/main/java/com/xt/start/StartApplication.java
@@ -1,9 +1,11 @@
 package com.xt.start;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.xt.start.mapper")
 public class StartApplication {
 
   public static void main(String[] args) {
